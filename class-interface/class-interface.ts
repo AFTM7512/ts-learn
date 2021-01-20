52a133754f9ce9a07245e23792ac2b861c1b311a
AFTM7512 <jmethods@163.com>
2021-01-20 17:52:02 +0800

    add-增加class内容

--- class-interface/class-interface.ts
+++ class-interface/class-interface.ts
@@ -1,10 +1,16 @@
 /**
  * 用 interface 定义 类的结构
  *   1. 使用 implements 关键字；
- *   2. 接口只能约束类的公有成员；
+ *   2. 接口只能约束类的公有成员，不包括私有成员和静态属性；
  *   3. 类可以定义接口中没有的属性和方法，包括私有成员；
  *   4. 接口不可以定义类的构造函数
  */
+interface Human {
+  readonly id: number,
+  name: string,
+  sleep(): void,
+}
+
 
 /**
  * 接口的继承：
--- class/chain-call.ts
+++ class/chain-call.ts
@@ -1,3 +1,36 @@
 /**
  * 类实例化后,实现内部方法的链式调用
+ *  链式调用的关键是再方法中返回 this，意思就是将实例对象返回
  */
+
+class Person {
+  constructor(id: number, name: string) {
+    this.id = id
+    this.name = name
+  }
+  id: number
+  name: string
+
+  eat() {
+    console.log(`${this.name} eat`);
+    return this
+  }
+
+  sleep() {
+    console.log(`${this.name} sleep`);
+    return this
+  }
+}
+
+new Person(1, '小明').eat().sleep()
+
+/**
+ * 子类继承父类后，覆写了父类的方法，此时再去调用，this 就实现了多肽
+ */
+class Man extends Person {
+  eat() {
+    console.log(`This is man, ${this.name} eat`);
+    return this
+  }
+}
+new Man(2, '小红').eat().sleep()
