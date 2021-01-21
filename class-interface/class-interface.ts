7ccbb163eb6f84ae4ea18a36af0c6d04b346baa4
AFTM7512 <jmethods@163.com>
2021-01-21 10:39:58 +0800

    add-完善 class 与 interface 的关系

--- class-interface/class-interface.ts
+++ class-interface/class-interface.ts
@@ -10,7 +10,30 @@ interface Human {
   name: string,
   sleep(): void,
 }
+class Person implements Human {
+  constructor(id: number, name: string) {
+    this.id = id
+    this.name = name
+  }
+  id: number
+  name: string
 
+  sleep(): void {
+    console.log('sleep');
+    /**
+     * interface 中定义的 readonly 再这里并没有生效
+     */
+    // this.id = 123 
+  }
+}
+
+/**
+ * 此时 Man 继承 Person，同时 Person 是使用 Human 约束的，
+ * 所以 Man 不需要
+ */
+class Man extends Person implements Human {
+
+}
 
 /**
  * 接口的继承：
@@ -19,8 +42,31 @@ interface Human {
  *      此时不仅抽离了类的公有成员，还抽离了私有...成员
  *  3.
  */
+interface Animal {
+  type: string,
+  eat(food: string): void
+}
+class TomCat implements Animal {
+  constructor(type: string) {
+    this.type = type
+  }
+  type: string
 
+  eat(fish: string): void {
+    console.log('eat' + fish);
+  }
 
-/**
- *
- */
+  climbTree() {}
+}
+
+// 接口 继承 类
+interface Cat extends TomCat {
+
+}
+let tom: Cat = {
+  type: 'cat',
+  eat(fish: string) {
+    console.log('eat' + fish);
+  },
+  climbTree() {}
+}
